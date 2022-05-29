f1093caaecc2aa8ca4dccca8c3c721069e6f6ecf
cgs <215966931@qq.com>
2022-05-29 21:27:27 +0800

    chore: vue3中的ts支持

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,25 @@
-<script setup lang="ts">
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
   <img
     alt="Vue logo"
     src="./assets/logo.png"
   >
-  <HelloWorld msg="Hello Vue 3 + TypeScript + Vite" />
+  <HelloWorld
+    ref="helloWorldRef"
+    msg="Hello Vue 3 + TypeScript + Vite"
+  />
 </template>
+
+<script setup lang="ts">
+// This starter template is using Vue 3 <script setup> SFCs
+// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
+import { onMounted, ref } from 'vue'
+import HelloWorld from './components/HelloWorld.vue'
+// 组件的模板引用类型声明
+const helloWorldRef = ref<InstanceType<typeof HelloWorld> | null>(null)
+onMounted(() => {
+  console.log('helloWorldRef', helloWorldRef.value)
+})
+</script>
 
 <style>
 #app {
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,14 +1,7 @@
-<script setup lang="ts">
-import { ref } from 'vue'
-
-defineProps<{ msg: string }>()
-
-const count = ref(0)
-</script>
-
 <template>
-  <h1>{{ msg }}</h1>
-
+  <h1 ref="titleRef">
+    {{ props.msg }}
+  </h1>
   <p>
     Recommended IDE setup:
     <a
@@ -21,9 +14,7 @@ const count = ref(0)
       target="_blank"
     >Volar</a>
   </p>
-
   <p>See <code>README.md</code> for more information.</p>
-
   <p>
     <a
       href="https://vitejs.dev/guide/features.html"
@@ -37,7 +28,6 @@ const count = ref(0)
       target="_blank"
     >Vue 3 Docs</a>
   </p>
-
   <button
     type="button"
     @click="count++"
@@ -49,6 +39,38 @@ const count = ref(0)
     <code>components/HelloWorld.vue</code> to test hot module replacement.
   </p>
 </template>
+
+<script setup lang="ts">
+import { onMounted, PropType, ref } from 'vue'
+
+interface User {
+  name: String,
+  age: Number
+}
+
+const props = defineProps({
+  msg: {
+    type: String,
+    default: ''
+  },
+  userObj: {
+    type: Object as PropType<User>,
+    default: () => {} // 提供一个默认值或者定义成必填项
+  }
+})
+
+const count = ref(0)
+const foo = ref<{a:Number, b: String} | null>(null)
+foo.value = {
+  a: 1,
+  b: ''
+}
+const titleRef = ref<HTMLHeadElement|null>(null)
+
+onMounted(() => {
+  console.log('titleRef', titleRef.value)
+})
+</script>
 
 <style scoped>
 a {
